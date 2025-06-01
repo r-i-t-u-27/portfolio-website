055d65bcbeb00e30392d6bf5a34a3e83dfacca58
r-i-t-u-27 <ritukumari272003@gmail.com>
2025-06-01 20:37:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Smooth scroll for anchor links (if used)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
